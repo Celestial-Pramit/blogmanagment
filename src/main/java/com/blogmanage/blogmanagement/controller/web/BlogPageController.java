27a0b64b0d67fffd3a47d036ffe31dfcb0ec1492
pramit <pramitdattaofficial04@gmail.com>
2026-09-23 22:28:14 +0600

    Fix blog-detail Thymeleaf error: remove forbidden static access T(System)

--- src/main/java/com/blogmanage/blogmanagement/controller/web/BlogPageController.java
+++ src/main/java/com/blogmanage/blogmanagement/controller/web/BlogPageController.java
@@ -47,6 +47,7 @@ public class BlogPageController {
             return "redirect:/auth/login";
         }
         model.addAttribute("post", post);
+        model.addAttribute("contentHtml", post.content().replace("\r\n", "\n").replace("\n", "<br/>"));
         return "blog-detail";
     }
 
